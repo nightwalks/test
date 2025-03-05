c9e1127d106634cfd885790e398de6d22287308b
nightwalks <156677315+nightwalks@users.noreply.github.com>
2025-03-05 12:53:35 +0000

    Add files via upload

---
+++ WorksheetTwo_3128600/src/griffith/Conversions.java
@@ -0,0 +1,26 @@
+package griffith;
+
+public class Conversions {
+    public double euroToDollar(double euro){
+        return 0;
+    }
+
+    public double dollarToEuro(double dollar){
+        return 0;
+    }
+
+    public int stringToInteger (String val){
+        return 0;
+    }
+
+    public String integerToString (int val){
+        return "";
+    }
+
+    // change uppercase to lowercase and vice versa
+    public String switchCase(){
+        return "";
+    }
+// Include screenshots of the method stubs below
+
+}
